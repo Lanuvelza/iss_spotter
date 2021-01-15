696fe20ce79e33deaf89a85e4a6e2e0c519f9fd7
Lanuvelza <ianlam1221@hotmail.com>
2021-01-15 01:39:18 +0000

    updated the function correctly

--- index2.js
+++ index2.js
@@ -8,7 +8,11 @@ const { nextISSTimesForMyLocation } = require('./iss_promised');
 // Call 
 nextISSTimesForMyLocation()
   .then((passTimes) => {
-    printPassTimes(passTimes);
+    for (let time of passTimes) { 
+      const date = new Date(0); 
+      date.setUTCSeconds(time.risetime);
+      console.log(`Next pass at ${date} for ${time.duration} seconds!`); 
+    };
   })
   .catch((error) => {
     console.log("It didn't work: ", error.message);
